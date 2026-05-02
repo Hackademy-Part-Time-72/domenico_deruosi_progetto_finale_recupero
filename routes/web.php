1b01b86dcc01a3f12401aead9beca835233f4f33
domederu2-commits <domederu2@gmail.com>
2026-05-02 22:05:36 +0200

    protezione rotte articles con middleware auth

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::resource('articles', ArticleController::class);
+});
