a8f039c8ea6780a4b553b3875864ce7d96139a7a
domederu2-commits <domederu2@gmail.com>
2026-05-02 20:48:57 +0200

    implementazione relazioni tra User, Article e Tag

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,7 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
