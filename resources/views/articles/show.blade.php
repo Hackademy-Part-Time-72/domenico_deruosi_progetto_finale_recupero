11a0e3ecdfceba03026aeb81449a8004944b0ca8
domederu2-commits <domederu2@gmail.com>
2026-05-09 22:33:35 +0200

    Migliorata la navigazione della dashboard e aggiunto accesso rapido all'area riservata per gli autori

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,13 +3,24 @@
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-9">
-        <div class="mb-4">
-            <a href="{{ route('homepage') }}" class="btn btn-outline-secondary shadow-sm px-4" style="color: #2d5a27; border-color: #2d5a27;">
+        <div class="mb-4 d-flex justify-content-between">
+            <a href="{{ route('articles.blog') }}" class="btn btn-outline-secondary shadow-sm px-4 rounded-pill">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left me-2" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
                 </svg>
-                Indietro
+                Torna al Blog
             </a>
+            @auth
+                @if($article->user_id == Auth::id())
+                    <a href="{{ route('articles.index') }}" class="btn btn-success shadow-sm px-4 rounded-pill">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-kanban me-2" viewBox="0 0 16 16">
+                            <path d="M13.5 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h11zm-11-1a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2h-11z"/>
+                            <path d="M6.5 3a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm-4 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm8 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3z"/>
+                        </svg>
+                        Gestisci in Area Riservata
+                    </a>
+                @endif
+            @endauth
         </div>
 
         <article class="bg-white p-5 rounded shadow-sm border" style="border-color: #d2b48c !important;">
